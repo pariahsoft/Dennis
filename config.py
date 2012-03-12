10a25a64d6f4ae7d6958a04156eea5986b53180b
Michael D. Reiley <mreiley@omegasdg.com>
2012-03-11 23:44:37 -0700

    Remove extraneous info from config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ config = {
 	"nick"      : "[OSDG]Dennis", # Nickname
 	"user"      : "dennis",  # Username
 	"real"      : "Dennis Alpha", # Realname
-	"nickpass"  : "Rhombus!", # Nickserv Password
+	"nickpass"  : "nspassword", # Nickserv Password
 	"worldfile" : "world.db", # World Database File
 	"timeout"   : 5, # Connection Timeout in Seconds
 	"retryrate" : 3, # Connection Retry Rate in Seconds
 
 	# ["HOST", PORT, "SERVPASS"]
 	"server"    : ["irc.somenetwork.net", 6667, ""], # Server to join.
-	
+
 	# [["CHANNEL", "PASS"], ["CHANNEL, "PASS"]]
-	"channels"  : [["#somechannel", ""]] # Channels to join.
+	"channels"  : [["#dennis", ""]] # Channels to join.
 }
 
